b550fbc863ad911be107493aa20729644b7577c2
Michael Bestas <mkbestas@lineageos.org>
2022-08-13 04:25:09 +0300

    comet: Copy touch modules to vendor_kernel_boot
    Makes touch work in recovery
    syna_touch requires qbt_handler, heatmap & goog_touch_interface symbols
    
    Co-authored-by: Nicholas Lim <nicknitewolf@lineageos.org>
    Co-authored-by: LuK1337 <priv.luk@gmail.com>
    Change-Id: Iaa5a158af827732b13ab79b60bed2f319ce56391

--- comet/device-lineage.mk
+++ comet/device-lineage.mk
@@ -12,3 +12,7 @@ PRODUCT_PACKAGES += \
     PixelDisplayServiceOverlayComet
 
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/$(DEVICE_CODENAME)/vendor.prop
+
+# Init
+PRODUCT_PACKAGES += \
+    init.recovery.comet.touch.rc
