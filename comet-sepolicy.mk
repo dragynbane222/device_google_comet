76a6fad054211247b1ef00266794cad260c2020c
Wilson Sung <wilsonsung@google.com>
2023-12-19 03:51:10 +0000

    Move fingerprint to each device sepolicy folder
    Bug: 312322769
    Test: make sepolicy
    Change-Id: I5f0032655f97e01cd18fdabb9d909e9e2295744d

--- comet-sepolicy.mk
+++ comet-sepolicy.mk
@@ -1,5 +1,2 @@
 # sepolicy exclusively for comet.
 BOARD_SEPOLICY_DIRS += device/google/comet-sepolicy/vendor
-
-# Fingerprint
-BOARD_SEPOLICY_DIRS += device/google/comet-sepolicy/fingerprint_capacitance
